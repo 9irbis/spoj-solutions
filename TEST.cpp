b48f8c651aa040395640d0747c62dd4cc1fbee91
Nakul <9irbis@gmail.com>
2015-08-23 18:48:11 +0530

    Life, the Universe and Everything

---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    while (cin >> x)
+    {
+        if (x == 42)
+            break;
+        cout << x << "\n";
+    }
+    return 0;
+}
