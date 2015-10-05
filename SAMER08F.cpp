cfac8dbf0efb9a52b3b2db18c9ef3adf36cee8f9
Nakul <9irbis@gmail.com>
2015-10-05 15:18:19 +0530

    Feynman

---
+++ SAMER08F.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n)
+    {
+        int sum = 0;
+        for(int i=1; i<=n; i++)
+            sum += i*i;
+        cout << sum << "\n";
+        cin >> n;
+    }
+    return 0;
+}
