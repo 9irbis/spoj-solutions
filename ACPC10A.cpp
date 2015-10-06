772224316a211da2b04cba0f0c7e16655cd9eb39
Nakul <9irbis@gmail.com>
2015-10-06 15:40:49 +0530

    To and fro

---
+++ ACPC10A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    while (a || b || c)
+    {
+        if (a == b == c)
+            cout << "GP " << c << "\n";
+        else if (2*b == a+c)
+            cout << "AP " << c+c-b << "\n";
+        else if (c/b == b/a)
+            cout << "GP " << c*(c/b) << "\n";
+        cin >> a >> b >> c;
+    }
+    return 0;
+}
