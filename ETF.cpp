512eb6a4df2ebd70fc4867271e6888fbfcbbd3df
Nakul <9irbis@gmail.com>
2015-11-18 18:20:49 +0530

    Euler totient function

---
+++ ETF.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t, n, ans;
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d", &n);
+        ans = n;
+        for(int i=2; i*i<=n; i++)
+        {
+            if (n%i == 0) 
+                ans -= ans/i;
+            while (n%i == 0)
+                n /= i;
+        }
+        if (n>1)
+            ans -= ans/n;
+        printf("%d\n", ans);
+    }
+    return 0;
+}
